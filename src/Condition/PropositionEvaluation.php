8dd1dbfc376ee4d0320851929e3dceb4a8718dc2
nicmart <nicmartnic@gmail.com>
2014-05-11 23:02:48 +0100

    Proposition evaluation class

---
+++ src/Condition/PropositionEvaluation.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * This file is part of library-template
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+namespace NicMart\Rulez\Condition;
+
+
+/**
+ * Class PropositionEvaluation
+ * @package NicMart\Rulez\Condition
+ */
+class PropositionEvaluation
+{
+    /**
+     * @var int
+     */
+    private $numOfMaps;
+
+    /**
+     * @var int
+     */
+    private $atLeast;
+
+    /**
+     * @var int
+     */
+    private $atMost;
+
+    /**
+     * @var callable
+     */
+    private $resolveCallback;
+
+    /**
+     * @var int
+     */
+    private $matched = 0;
+
+    /**
+     * @var int
+     */
+    private $remainingMaps;
+
+    /**
+     * @var bool|null
+     */
+    private $resolvedStatus = null;
+
+    /**
+     * @param $numOfMaps
+     * @param $atLeast
+     * @param $atMost
+     * @param callable $resolveCallback
+     */
+    function __construct($numOfMaps, $atLeast, $atMost, callable $resolveCallback)
+    {
+        $this->numOfMaps = $this->remainingMaps = $numOfMaps;
+        $this->atLeast = $atLeast;
+        $this->atMost = $atMost;
+        $this->resolveCallback = $resolveCallback;
+    }
+
+    /**
+     * @return $this
+     */
+    function signalMatch()
+    {
+        $this->matched++;
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    function signalMapUsed()
+    {
+        $this->remainingMaps--;
+        $maxMatches = $this->matched + $this->remainingMaps;
+
+        if ($maxMatches < $this->atLeast)
+            return $this->resolve(false);
+        if ($this->atLeast <= $this->matched && $maxMatches <= $this->atMost)
+            return $this->resolve(true);
+        if ($this->matched > $this->atMost)
+            return $this->resolve(false);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    function reset()
+    {
+        $this->matched = 0;
+        $this->remainingMaps = $this->numOfMaps;
+        $this->resolvedStatus = null;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    function isResolved()
+    {
+        return $this->resolvedStatus !== null;
+    }
+
+    function resolvedStatus()
+    {
+        if (!$this->isResolved())
+            throw new \LogicException("The proposition is not resolved yet.");
+
+        return $this->resolvedStatus;
+    }
+
+    /**
+     * @param bool $resolvedStatus
+     *
+     * @return $this
+     */
+    private function resolve($resolvedStatus)
+    {
+        $this->resolvedStatus = $resolvedStatus;
+        $callback = $this->resolveCallback;
+        $callback($resolvedStatus, $this);
+
+        return $this;
+    }
+} 
---
+++ tests/Condition/PropositionEvaluationTest.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * This file is part of library-template
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+namespace NicMart\Rulez\Test\Condition;
+
+use NicMart\Rulez\Condition\PropositionEvaluation;
+
+class PropositionEvaluationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAnOR()
+    {
+        $callback = function($status) { };
+
+        // An OR, with only two maps
+        $evaluation = new PropositionEvaluation(2, 1, INF, $callback);
+        $this->assertFalse($evaluation->isResolved());
+
+        $evaluation->signalMapUsed();
+        $this->assertFalse($evaluation->isResolved());
+
+        $this->setExpectedException("\\LogicException");
+        $evaluation->resolvedStatus();
+
+        $evaluation->signalMapUsed();
+        $this->assertTrue($evaluation->isResolved());
+        $this->assertFalse($evaluation->resolvedStatus());
+
+        $evaluation->reset();
+        $this->assertFalse($evaluation->isResolved());
+
+        $evaluation->signalMatch();
+        $evaluation->signalMapUsed();
+        $this->assertTrue($evaluation->isResolved());
+        $this->assertTrue($evaluation->resolvedStatus());
+    }
+
+    public function testAnAnd()
+    {
+        $callback = function($status) { };
+
+        // An OR, with only two maps
+        $evaluation = new PropositionEvaluation(3, 3, INF, $callback);
+        $this->assertFalse($evaluation->isResolved());
+
+        $evaluation->signalMapUsed();
+        $this->assertTrue($evaluation->isResolved());
+        $this->assertFalse($evaluation->resolvedStatus());
+
+        $evaluation->reset();
+
+        // First condition
+        $evaluation->signalMatch();
+        $evaluation->signalMapUsed();
+        $this->assertFalse($evaluation->isResolved());
+
+        // Second
+        $evaluation->signalMatch();
+        $evaluation->signalMapUsed();
+        $this->assertFalse($evaluation->isResolved());
+
+        // Third
+        $evaluation->signalMatch();
+        $evaluation->signalMapUsed();
+        $this->assertTrue($evaluation->isResolved());
+        $this->assertTrue($evaluation->resolvedStatus());
+    }
+
+    public function testANot()
+    {
+        $callback = function($status) { };
+
+        // An OR, with only two maps
+        $evaluation = new PropositionEvaluation(3, -INF, 0, $callback);
+        $this->assertFalse($evaluation->isResolved());
+
+        $evaluation->signalMapUsed();
+        $this->assertFalse($evaluation->isResolved());
+
+        $evaluation->signalMatch();
+        $evaluation->signalMapUsed();
+        $this->assertTrue($evaluation->isResolved());
+        $this->assertFalse($evaluation->resolvedStatus());
+
+        $evaluation->reset();
+
+        // First condition
+        $evaluation->signalMapUsed();
+        $this->assertFalse($evaluation->isResolved());
+
+        // Second
+        $evaluation->signalMapUsed();
+        $this->assertFalse($evaluation->isResolved());
+
+        // Third
+        $evaluation->signalMapUsed();
+        $this->assertTrue($evaluation->isResolved());
+        $this->assertTrue($evaluation->resolvedStatus());
+    }
+}
+ 
